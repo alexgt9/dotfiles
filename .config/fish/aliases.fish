9c4d3c59db9f2826b4e0c3ad29b6addd5a02c317
Alejandro Perez <alexgt9@gmail.com>
2013-09-23 11:11:13 +0200

    Initial commit

---
+++ .config/fish/aliases.fish
@@ -0,0 +1,22 @@
+alias l ls
+alias ll 'ls -la'
+alias g git
+alias c clear
+alias v vim
+
+#Github
+alias gti git
+alias gest 'git status'
+alias ggpush 'git push origin (git_current_branch)'
+alias ggpull 'git pull origin (git_current_branch)'
+
+#Directories
+
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
+alias ..... 'cd ../../../..'
+
+#PHP
+
+alias pa 'php -a'
---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+. ~/.config/fish/aliases.fish
+
+function git_current_branch -d 'Prints a human-readable representation of the current branch'
+  set -l ref (git symbolic-ref HEAD ^/dev/null; or git rev-parse --short HEAD ^/dev/null)
+  if test -n "$ref"
+    echo $ref | sed -e s,refs/heads/,,
+    return 0
+  end
+end
